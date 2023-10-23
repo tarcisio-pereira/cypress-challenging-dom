cfa7689db4d757fec4c49cd97fb3a40efc998a27
Tarcísio Pereira <tarcisio.95@hotmail.com>
2023-10-22 22:56:17 -0300

    feat: colorful buttons

---
+++ cypress/e2e/dom.cy.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+
+describe('Teste de manipulação de Dom', () => {
+    beforeEach(() => {
+      cy.visit('https://the-internet.herokuapp.com/challenging_dom')
+      cy.get('title').should('have.text', 'The Internet')
+    })
+
+    it('Visite a página inicial', ()=> {
+        cy.get('h3').should('have.text', 'Challenging DOM')
+    })
+
+    it('Clicar no botão Azul', ()=> {     
+        cy.get('.button')
+        .eq(0)    
+        .click()
+    })
+
+    it('Clicar no botão Vermelho', ()=> {
+        cy.get('.button')
+        .eq(1)    
+        .click()
+    })
+
+    it('Clicar no botão Verde', ()=> {     
+        cy.get('.button')
+        .eq(2)    
+        .click()
+    })
+
+    //Pendente
+    // it.only('Clicar em todos os botões "edit"', ()=> {     
+    //     cy.get('a')
+    //     parent()
+    //     find('button[href=edit]')
+    //     .eq(0)
+    //     .click()
+    // })
+
+})
