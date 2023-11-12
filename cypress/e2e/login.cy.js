cd17fec28b02fa686f2be8f67e6fff37f97d573e
Tarcísio Pereira <tarcisio.95@hotmail.com>
2023-11-11 21:19:59 -0300

    feat: Challengig Dom Full

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,50 +9,33 @@ describe('Login', () => {
 
      it('[ATH-O1] Validar entrar na área segura', ()=> {     
        
-        cy.get('#username').type('tomsmith')
-
-        cy.get('#password').type('SuperSecretPassword!')  
-
-        cy.get('button[type="submit"]').click()
+        cy.submitUsernamePassword('tomsmith', 'SuperSecretPassword!')
 
         cy.get('#flash').should('be.visible', 'You logged into a secure area!')
       })
 
     it('[ATH-O2] Validar nome do usuário inválido', ()=> {     
-       
-        cy.get('#username').type('tomsmiti')
-
-        cy.get('#password').type('SuperSecretPassword!')  
-
-        cy.get('button[type="submit"]').click()
+ 
+        cy.submitUsernamePassword('tomsmiti', 'SuperSecretPassword!')
 
         cy.get('#flash').should('be.visible', 'Your username is invalid!')
      })
 
       it('[ATH-O3] Validar senha inválida', ()=> {     
        
-         cy.get('#username').type('tomsmith')
-   
-         cy.get('#password').type('SuperSecretPassword')  
-   
-         cy.get('button[type="submit"]').click()
+         cy.submitUsernamePassword('tomsmith', 'SuperSecretPassword')
    
          cy.get('#flash').should('be.visible', 'Your password is invalid!')
       })
 
       it('[ATH-O4] Validar saída da área segura', ()=> {     
        
-         cy.get('#username').type('tomsmith')
-   
-         cy.get('#password').type('SuperSecretPassword!')  
-   
-         cy.get('button[type="submit"]').click()
+         cy.submitUsernamePassword('tomsmith', 'SuperSecretPassword!')
    
          cy.get('#flash').should('be.visible', 'You logged into a secure area!')
 
-         cy.get('.button').click()
+         cy.contains('a', 'Logout').click()
 
          cy.get('#flash').should('be.visible', 'You logged out of the secure area!')
       })
-
 })
